302c4ac123ea4d42fff550f9bb579b26395808cc
Mark Oteiza <mvoteiza@udel.edu>
2013-02-08 02:36:11 -0500

    zsh: tweak vcs prompt

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -31,7 +31,7 @@ autoload -Uz vcs_info
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' stagedstr     '%B%F{green}+%f%b'
 zstyle ':vcs_info:*' unstagedstr   '%B%F{yellow}*%f%b'
-zstyle ':vcs_info:*' formats       '%B%F{black}(%c%u%%b%F{9}%b%m%B%F{black})'
+zstyle ':vcs_info:*' formats       '%B%F{black}(%c%u%%b%F{9}%b%f%m%B%F{black})'
 #zstyle ':vcs_info:*' actionformats '%c%u%b%m %B%s-%a%%b'
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-aheadbehind git-remotebranch
 
@@ -44,7 +44,7 @@ function +vi-git-untracked(){
         # If instead you want to show the marker only if there are untracked
         # files in $PWD, use:
         #[[ -n $(git ls-files --others --exclude-standard) ]] ; then
-        hook_com[unstaged]+='%B%F{white}?%f%b'
+        hook_com[unstaged]+='%B%F{white}?%b%f'
     fi
 }
 
@@ -80,8 +80,8 @@ function +vi-git-remotebranch() {
     # second test, however, will only show the remote branch's name if it
     # differs from the local one.
     #if [[ -n ${remote} ]] ; then
-    if [[ -n ${remote} && ${remote#*/} != ${hook_com[branch]} ]] ; then
-        hook_com[branch]="${hook_com[branch]}(%F{cyan}${remote}%f)"
-    fi
+    #if [[ -n ${remote} && ${remote#*/} != ${hook_com[branch]} ]] ; then
+		#	hook_com[branch]="${hook_com[branch]}%F{cyan}(${remote})%f"
+    #fi
 }
 
