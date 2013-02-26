500f8fae999ab094ae96e7812ccde5744c702e89
Mark Oteiza <mvoteiza@udel.edu>
2013-02-26 13:21:29 -0500

    zsh: pz ls++

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -12,8 +12,8 @@ alias rm='rm -Iv'
 alias ln='ln -iv'
 alias ls='ls -hF --color=auto --group-directories-first'
 alias la='ls -A'
-alias ll="ls++ --potsf"
-alias grep="grep --color=auto"
+alias ll='ls -l'
+alias grep='grep --color=auto'
 
 # pastebins
 alias ix="curl -F 'f:1=<-' http://ix.io"
