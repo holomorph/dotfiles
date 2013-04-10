32811e8be63a7f21915c0b86c4da8fb026a8ab1d
Mark Oteiza <mvoteiza@udel.edu>
2013-04-10 15:39:30 -0400

    terminfo: slightly better bloat

--- .terminfo/terminfo.sh
+++ .terminfo/terminfo.sh
@@ -12,29 +12,33 @@ GREEN="${BOLD}\e[1;32m"
 RED="${BOLD}\e[1;31m"
 YELLOW="${BOLD}\e[1;33m"
 
-printf "${BLUE}>>${ALL_OFF}${BOLD} Checking existing $TERMINFO...${ALL_OFF}\n"
+echo -e "${BLUE}>>${ALL_OFF}${BOLD} Checking existing $TERMINFO...${ALL_OFF}"
 if [[ -f "$HOME/.terminfo/s/$TERMINFO" ]]; then
-	printf "${YELLOW}   >>${ALL_OFF} User $TERMINFO already exists!\n"
+	echo -e "${YELLOW}   >>${ALL_OFF} User $TERMINFO already exists!"
 	cd "$HOME/.terminfo/"
-	printf "${YELLOW}   >>${ALL_OFF} Removing existing terminfo...\n"
+	echo -e "${YELLOW}   >>${ALL_OFF} Removing existing terminfo..."
 	rm -f "s/$TERMINFO"
 	rmdir "s/"
 else
-	printf "${YELLOW}   >>${ALL_OFF} $TERMINFO does not exist\n"
+	echo -e "${YELLOW}   >>${ALL_OFF} $TERMINFO does not exist"
 fi
 
 if [[ ! -d "$HOME/.terminfo" ]]; then
-	printf "${BLUE}>>${ALL_OFF}${BOLD} Creating .terminfo directory... ${ALL_OFF}\n"
+	echo -e "${BLUE}>>${ALL_OFF}${BOLD} Creating .terminfo directory... ${ALL_OFF}"
 	mkdir "$HOME/.terminfo/"
 fi
 
-printf "${BLUE}>>${ALL_OFF}${BOLD} Patching $TERMINFO... ${ALL_OFF}\n"
+echo -e "${BLUE}>>${ALL_OFF}${BOLD} Patching $TERMINFO... ${ALL_OFF}"
 infocmp "$TERMINFO" | sed \
 -e 's/^screen[^|]*|[^,]*,/screen-256color|screen-256color with italics support,/' \
 -e 's/%?%p1%t;3%/%?%p1%t;3%/' \
 -e 's/smso=[^,]*,/smso=\\E[7m,/' \
 -e 's/rmso=[^,]*,/rmso=\\E[27m,/' \
 -e '$s/$/ sitm=\\E[3m, ritm=\\E[23m,/' > "$HOME/.terminfo/"$TERMINFO".terminfo"
-printf "${BLUE}>>${ALL_OFF}${BOLD} Compiling new terminfo file... ${ALL_OFF}\n"
+echo -e "${BLUE}>>${ALL_OFF}${BOLD} Compiling new terminfo file... ${ALL_OFF}"
 tic "$HOME/.terminfo/"$TERMINFO".terminfo"
-printf "${GREEN}>>${ALL_OFF}${BOLD} Done! ${ALL_OFF}\n"
+echo -e "${GREEN}>>${ALL_OFF}${BOLD} Done! ${ALL_OFF}"
+
+unset ALL_OFF BOLD BLUE GREEN RED YELLOW
+unset TERMINFO
+unset CDPATH
