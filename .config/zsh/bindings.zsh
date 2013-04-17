7e7fb7da62cbc563befca41a0bea4f092b62d451
Mark Oteiza <mvoteiza@udel.edu>
2013-04-17 00:20:59 -0400

    zsh: emacs bindings

--- .config/zsh/bindings.zsh
+++ .config/zsh/bindings.zsh
@@ -1,33 +1,21 @@
 # ~/.config/zsh/bindings.zsh
 
+bindkey -e # emacs
+
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-bindkey "\e[1~" beginning-of-line # Home (tmux)
-bindkey "\e[2~" quoted-insert # Ins
-bindkey "\e[3~" delete-char # Del
-bindkey "\e[4~" end-of-line # End (tmux)
-bindkey "\e[5~" beginning-of-history # PgUp
-bindkey "\e[6~" end-of-history # PgDn
-bindkey "\e[7~" beginning-of-line # Home (rxvt)
-bindkey "\e[8~" end-of-line # End (rxvt)
-bindkey "\e[Z" reverse-menu-complete # Shift+Tab
-bindkey "^[[A" up-line-or-beginning-search # Up
-bindkey "^[[B" down-line-or-beginning-search # Down
-
-bindkey "^A" beginning-of-line
-bindkey "^E" end-of-line
-bindkey "^U" kill-whole-line
-bindkey "^B" backward-word
-bindkey "^F" forward-word
 bindkey " " magic-space
+bindkey "^[[A" up-line-or-beginning-search   # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
+bindkey "^[[Z" reverse-menu-complete         # Shift+Tab
+bindkey "^A" beginning-of-line
+bindkey "^B" backward-word
+bindkey "^E" end-of-line
+bindkey "^F" forward-word
+bindkey "^K" kill-line
+bindkey "^N" down-line-or-beginning-search
+bindkey "^P" up-line-or-beginning-search
+bindkey "^U" kill-whole-line
+bindkey "^W" vi-backward-kill-word
 bindkey "^?" backward-delete-char
-#bindkey -M viins "^N" down-line-or-beginning-search
-#bindkey -M viins "^P" up-line-or-beginning-search
-#bindkey -M viins "jj" vi-cmd-mode
-#bindkey -M vicmd "^R" redo
-#bindkey -M vicmd "u" undo
-#bindkey -M vicmd "/" history-incremental-search-forward
-#bindkey -M vicmd "?" history-incremental-search-backward
-
-
