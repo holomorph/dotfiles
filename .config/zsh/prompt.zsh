1efc2f25ca7812729b08243be2fcb2b69bc49317
Mark Oteiza <mvoteiza@udel.edu>
2013-10-14 12:27:51 -0400

    zsh: git status update
    * change ordering
    * add stash indicator

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -13,24 +13,30 @@ zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' stagedstr     '%B%F{green}+%f%b'
 zstyle ':vcs_info:*' unstagedstr   '%B%F{yellow}*%f%b'
-zstyle ':vcs_info:*' formats       '%c%u%F{9}%b%f%m '
-zstyle ':vcs_info:*' actionformats '%c%u%F{9}%b%f%m %F{14}%a%f '
-zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-aheadbehind git-remotebranch
+zstyle ':vcs_info:*' formats       '%F{9}%b%f%c%u%m '
+zstyle ':vcs_info:*' actionformats '%F{9}%b%f%c%u%m %F{14}%a%f '
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-aheadbehind git-remotebranch git-stash
 
 ### git: Show marker if there are untracked files in repository
-# Make sure you have added staged to your 'formats':  %c
+# Make sure staged is in 'formats':  %c
 +vi-git-untracked() {
-  if git status --porcelain | grep '??' &>/dev/null; then
   # This will show the marker if there are any untracked files in repo.
-  # If instead you want to show the marker only if there are untracked
-  # files in $PWD, use:
-  # if [[ -n $(git ls-files --others --exclude-standard) ]]; then
-    hook_com[unstaged]+='%B%F{white}^%b%f'
+  if [[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" == 'true' ]]; then
+    git status --porcelain | grep -L '^??' &>/dev/null && \
+      hook_com[unstaged]+='%B%F{white}^%b%f'
   fi
 }
 
-### git: Show +N/-N when your local branch is ahead-of or behind remote HEAD.
-# Make sure you have added misc to your 'formats':  %m
+### git: Show stash existence
+# Make sure misc is in 'formats':  %m
++vi-git-stash() {
+  if [[ -s ${hook_com[base]}/.git/refs/stash ]]; then
+    hook_com[misc]+="%B%F{14}#%b%f"
+  fi
+}
+
+### git: Show +N-M when the local branch is N ahead or M behind remote HEAD.
+# Make sure misc is in 'formats':  %m
 +vi-git-aheadbehind() {
   local ahead behind
   local -a gitstatus
@@ -44,12 +50,10 @@ zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-aheadbehind git-re
 }
 
 ### git: Show remote branch name for remote-tracking branches
-# Make sure you have added staged to your 'formats':  %b
+# Make sure branch is in 'formats':  %b
 +vi-git-remotebranch() {
-  local remote
-
   # Are we on a remote-tracking branch?
-  remote=${$(git rev-parse --verify ${hook_com[branch]}@{upstream} \
+  local remote=${$(git rev-parse --verify ${hook_com[branch]}@{upstream} \
       --symbolic-full-name 2>/dev/null)/refs\/remotes\/}
 
   # The first test will show a tracking branch whenever there is one. The
@@ -57,6 +61,6 @@ zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-aheadbehind git-re
   # differs from the local one.
   # if [[ -n ${remote} ]] ; then
   if [[ -n ${remote} && ${remote#*/} != ${hook_com[branch]} ]]; then
-    hook_com[branch]="${hook_com[branch]}%F{cyan}(${remote})%f"
+    hook_com[branch]="${hook_com[branch]}%F{13}(${remote})%f"
   fi
 }
