5c24779cb5b5ad34e230210984fae70053743154
Mark Oteiza <mvoteiza@udel.edu>
2013-11-13 19:38:38 -0500

    dactyl: add video launching plugin @Earnestly
    update term colours in more of the places where they are used

---
+++ .config/pentadactyl/plugins/launchv.js
@@ -0,0 +1,22 @@
+function launchv(target) {
+    var uri = target.replace(/([$`"\\])/g, "\\$1");
+    var cmd;
+
+    function exec(launcher, uri) {
+        return commands.execute(launcher + ' "' + uri + '" &');
+    }
+
+    /* filter certain urls to more appropriate programs before passing to
+     * quvi */
+    if(uri.match(/twitch\.tv/))
+        exec("!lstream", uri);
+    else if(uri.match(/playlist\?list=PL/))
+        exec("!mpv", uri );
+    else
+        exec("!quvi dump -b mute", uri);
+}
+
+hints.addMode("l", "Launch video from hint", function (elem, loc) launchv(loc));
+
+group.commands.add(["launchv", "lv"], "Launches current buffer video.",
+    function(args) { launchv(buffer.URL); });
