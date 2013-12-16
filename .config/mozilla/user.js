f30ff9abb8818f8160cca1e77be8c929ca7ad47e
Mark Oteiza <mvoteiza@udel.edu>
2013-12-15 23:27:03 -0500

    systemd: pulse service
    mozilla: no smooth scroll please
    vim: gui font size
    xinitrc: nix things

--- .config/mozilla/user.js
+++ .config/mozilla/user.js
@@ -1,4 +1,5 @@
 user_pref("browser.tabs.onTop", false);
+user_pref("general.smoothScroll", false);
 user_pref("plugin.default.state", 1);
 user_pref("plugins.click_to_play", true);
 user_pref("plugins.hide_infobar_for_missing_plugin", true);
