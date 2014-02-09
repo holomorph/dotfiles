d66854379efa1a94ec41b8b835faaa2d53d45bd8
Mark Oteiza <mvoteiza@udel.edu>
2014-02-09 16:14:31 -0500

    fontconfig: prefer libertine otf
    zsh: llpp completion

--- .config/zsh/fun.zsh
+++ .config/zsh/fun.zsh
@@ -3,4 +3,9 @@
 n() {
   $EDITOR "${@[@]/#/"$HOME/doc/notes/"}";
 }
+
+## add notes completion, thanks to Earnestly
 compdef "_files -W ~/doc/notes -/" n
+
+## add pdf completion for llpp
+compdef "_pdf" llpp
