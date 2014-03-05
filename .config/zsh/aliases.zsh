26958904f137073794f3f9a8e64aa168d7e31ac9
Mark Oteiza <mvoteiza@udel.edu>
2014-03-05 12:29:35 -0500

    zsh: gdb and directory stack aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -5,6 +5,7 @@ alias g='git'
 alias e='emacsclient -t'
 alias z='zathura --fork'
 
+alias gdb='gdb -q'
 alias ncmpcpp='ncmpcpp -c ~/.config/ncmpcpp/config'
 alias weechat='weechat -d ~/.config/weechat'
 
@@ -17,6 +18,9 @@ alias ln='ln -iv'
 alias ls='ls -hF --color=auto --group-directories-first'
 alias la='ls -A'
 alias ll='ls -l'
+alias po='popd'
+alias pu='pushd'
+alias dirs='dirs -p'
 
 # pastebins
 alias ix="curl -F 'f:1=<-' http://ix.io"
