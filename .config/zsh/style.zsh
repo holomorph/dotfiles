af3d2d9afe0c92ac08045935f04dcc339121b7c1
Mark Oteiza <mvoteiza@udel.edu>
2014-07-25 23:43:42 -0400

    zsh: simplify style

--- .config/zsh/style.zsh
+++ .config/zsh/style.zsh
@@ -4,12 +4,12 @@ zstyle ':completion:*' menu select
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' rehash yes
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
-zstyle ':completion:*' completer _complete _match
-zstyle ':completion:*:match:*' original only
 zstyle ':completion:*' ignore-parents parent pwd
-zstyle ':completion:*' matcher-list 'r:|.=* r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' squeeze-slashes true
 zstyle ':completion:*:functions' ignored-patterns '_*'
+
+# ssh/scp/rsync
+zstyle ':completion:*:(ssh|scp|rsync):*:hosts' ignored-patterns 'localhost*' loopback
 
 # kill
 zstyle ':completion:*:*:kill:*:processes' command "ps -u$USER -o pid,%cpu,tty,cmd"
@@ -24,17 +24,3 @@ zstyle ':completion:*:*:zathura:*:*' file-patterns \
 zstyle ':completion:*:*:*:users' ignored-patterns \
 	bin daemon mail ftp http uuidd dbus nobody 'systemd*' avahi git bitlbee mpd \
 	dnsmasq colord rtkit ntp polkitd sagemath xbmc
-
-# ssh/scp/rsync
-zstyle ':completion:*:(scp|rsync):*' group-order \
-	files all-files hosts-domain hosts-host hosts-ipaddr
-zstyle ':completion:*:ssh:*' group-order \
-	hosts-domain hosts-host users hosts-ipaddr
-zstyle ':completion:*:(ssh|scp|rsync):*:hosts' ignored-patterns \
-	'*.*' loopback localhost
-zstyle ':completion:*:(ssh|scp|rsync):*:hosts-host' ignored-patterns \
-	'*.*' loopback localhost
-zstyle ':completion:*:(ssh|scp|rsync):*:hosts-domain' ignored-patterns \
-	'<->.<->.<->.<->' '^*.*' '*@*'
-zstyle ':completion:*:(ssh|scp|rsync):*:hosts-ipaddr' ignored-patterns \
-	'^<->.<->.<->.<->' '127.0.0.<->'
