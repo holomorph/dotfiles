a44f7e0618c9a5e382fb70d70f07598aad8cff2e
Mark Oteiza <mvoteiza@udel.edu>
2014-08-09 12:06:18 -0400

    emacs: no horizontal scroll bars

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,6 +6,7 @@
 
 (load-theme 'zenburn t)
 
+(when (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
