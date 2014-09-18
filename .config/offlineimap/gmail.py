f5d238c8265d546d9076a151bd56899978292082
Mark Oteiza <mvoteiza@udel.edu>
2014-09-18 16:10:09 -0400

    python nitpicks

--- .config/offlineimap/gmail.py
+++ .config/offlineimap/gmail.py
@@ -3,23 +3,24 @@
 
 import re
 
-mapping = { 'INBOX':              'inbox'
-#         , '[Gmail]/All Mail':   'archive'
-          , '[Gmail]/Drafts':     'drafts'
-#         , '[Gmail]/Important':  'important'
-          , '[Gmail]/Sent Mail':  'sent'
-          , '[Gmail]/Spam':       'spam'
-#         , '[Gmail]/Starred':    'starred'
-          , '[Gmail]/Trash':      'trash'
-          }
+mapping = {'INBOX': 'inbox',
+           '[Gmail]/All Mail': 'archive',
+           '[Gmail]/Drafts': 'drafts',
+           '[Gmail]/Important': 'important',
+           '[Gmail]/Sent Mail': 'sent',
+           '[Gmail]/Spam': 'spam',
+           '[Gmail]/Starred': 'starred',
+           '[Gmail]/Trash': 'trash'}
 
-r_mapping = { val: key for key, val in mapping.items() }
+r_mapping = {val: key for key, val in mapping.items()}
+
 
 def nt_remote(folder):
     try:
         return mapping[folder]
     except:
         return re.sub(' ', '_', folder).lower()
+
 
 def nt_local(folder):
     try:
