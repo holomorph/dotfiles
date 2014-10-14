9761832469c5e6d9877da0fddec6ff65f1d653ce
Mark Oteiza <mvoteiza@udel.edu>
2014-10-14 13:13:21 -0400

    emacs: disable flycheck makefile checker

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -138,7 +138,7 @@
  company-selection-wrap-around t
  company-show-numbers t
  flycheck-check-syntax-automatically '(save)
- flycheck-disabled-checkers '(emacs-lisp-checkdoc)
+ flycheck-disabled-checkers '(emacs-lisp-checkdoc make)
  flycheck-display-errors-delay 0.1
  ledger-highlight-xact-under-point nil
  magit-auto-revert-mode-lighter nil
