ff5a462ed8c55f8032a659739b11db5fb82006ac
Mark Oteiza <mvoteiza@udel.edu>
2014-12-21 11:37:05 -0500

    mozilla: add some security stuff
    https://github.com/pyllyukko/user.js

--- .config/mozilla/user.js
+++ .config/mozilla/user.js
@@ -1,10 +1,16 @@
 user_pref("browser.tabs.onTop", false);
 user_pref("general.smoothScroll", false);
-user_pref("media.peerconnection.enabled", false);
 user_pref("plugin.default.state", 1);
 user_pref("plugins.click_to_play", true);
 user_pref("plugins.hide_infobar_for_missing_plugin", true);
 user_pref("signon.rememberSignons", false);
+
+// hardening
+user_pref("browser.send_pings", false);
+user_pref("media.peerconnection.enabled", false);
+user_pref("network.http.sendRefererHeader", 0);
+user_pref("security.ask_for_password", 0);
+user_pref("security.cert_pinning.enforcement_level", 2);
 
 // show unsafe SSL negotiation
 user_pref("security.ssl.require_safe_negotiation", false);
