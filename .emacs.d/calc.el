83dfc792bcb883b62888ddb04bcd44cc1ad59b02
Mark Oteiza <mvoteiza@udel.edu>
2017-07-22 21:06:29 -0400

    emacs: add calc config

---
+++ .emacs.d/calc.el
@@ -0,0 +1,19 @@
+
+;;; Mode settings stored by Calc on Fri Jul  7 17:54:25 2017
+(setq calc-show-banner nil)
+(setq calc-group-digits t)
+;;; End of mode settings
+
+
+;;; Custom units stored by Calc on Tue Apr 19 10:52:25 2016
+(setq math-additional-units '(
+  (bit nil "Bit")
+  (byte "8 * bit" "Byte")
+  (Φ0 "Phi0" "Magnetic flux quantum")
+  (Phi0 "h / (2 * ech)" "Magnetic flux quantum")
+  (ly "lyr" "Light Year")
+  (Z0 "376.730313461 * ohm" "characteristic impedance of vacuum")
+  (RK "h / ech^2" "von Klitzing constant")
+))
+;;; End of custom units
+
