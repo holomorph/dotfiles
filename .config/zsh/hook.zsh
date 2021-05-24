ee0693dd11315cafeaa14600e64b4ed82a9bbee6
Mark Oteiza <mvoteiza@udel.edu>
2021-05-24 14:48:29 -0400

    alacritty: hints

--- .config/zsh/hook.zsh
+++ .config/zsh/hook.zsh
@@ -6,10 +6,15 @@ __print_title() {
   print -Pn "\e];%n@%m (%~) - Terminal\a"
 }
 
+__print_title_alacritty() {
+  print -Pn "\e]0;%n@%m (%~) - Terminal\a"
+}
+
 chpwd() {
   ls
 }
 
 case $TERM in
   ( rxvt* | vte* | xterm* ) precmd_functions+=(__print_title) ;;
+  ( alacritty ) precmd_functions+=(__print_title_alacritty) ;;
 esac
